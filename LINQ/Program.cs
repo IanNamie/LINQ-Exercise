e134a6da48142fbfff3ada10d4249950492bc900
Ian Namie <Iannamie82@gmail.com>
2023-03-23 10:51:21 -0500

    added and sorted name of games

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace LINQ
+﻿using System;
+using LINQ;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*reate a list of video game names...
+            Order the list of games by length of the game name.
+            Use the lambda expression in this exercise as well.
+            Use Method Syntax for this exercise*/
+
+            List<string> GameName = new List<string>();
+            GameName.Add("Elden Ring");
+            GameName.Add("Modern Warfare 2");
+            GameName.Add("For Honor");
+
+           var orderedGameName = GameName.OrderBy(name  => name.Length);
+            foreach (string name in orderedGameName) 
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("_____________________________________________________________");
+            var orderedGameName1 = GameName.OrderByDescending(name => name.Length);
+            foreach (string name1 in orderedGameName1) 
+            {
+                Console.WriteLine(name1);
+            }
         }
     }
 }
